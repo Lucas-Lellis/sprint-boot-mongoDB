221e68a856c3f6ac7eef1145ec755ad199a90e2c
Lucas Léllis <125090781+Lucas-Lellis@users.noreply.github.com>
2025-08-14 15:45:51 -0300

    feat: primeiro commit (criacao do projeto)

---
+++ src/test/java/net/codejava/sprint_boot_mongoDB/SprintBootMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package net.codejava.sprint_boot_mongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
