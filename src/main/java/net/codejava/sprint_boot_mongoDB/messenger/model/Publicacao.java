5a24aed60e911d2c6b6507c06379c9ae5c38507f
Lucas Léllis <125090781+Lucas-Lellis@users.noreply.github.com>
2025-08-14 16:33:27 -0300

    feat: classes models

---
+++ src/main/java/net/codejava/sprint_boot_mongoDB/messenger/model/Publicacao.java
@@ -0,0 +1,71 @@
+package net.codejava.sprint_boot_mongoDB.messenger.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Document(collation = "publicacoes")
+public class Publicacao {
+
+    @Id
+    private Long id;
+
+    private Date data;
+    private String titulo;
+    private String corpo;
+
+    public Publicacao() {
+    }
+
+    public Publicacao(Date data, String titulo, String corpo) {
+        this.data = data;
+        this.titulo = titulo;
+        this.corpo = corpo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getCorpo() {
+        return corpo;
+    }
+
+    public void setCorpo(String corpo) {
+        this.corpo = corpo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Publicacao that = (Publicacao) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/net/codejava/sprint_boot_mongoDB/messenger/model/Usuario.java
@@ -0,0 +1,60 @@
+package net.codejava.sprint_boot_mongoDB.messenger.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Objects;
+
+@Document(collection = "usuarios")
+public class Usuario {
+
+    @Id
+    private Long id;
+
+    private String nome;
+    private String email;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(id, usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
